02505702c7d987efda1a2d24b075306a76afbe2c
thelordman <101205562+thelordman@users.noreply.github.com>
2022-04-24 02:48:04 +0300

    Create HelpCommand.java

---
+++ src/main/java/io/github/thelordman/costrength/commands/HelpCommand.java
@@ -0,0 +1,28 @@
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import net.md_5.bungee.api.ChatColor;
+
+public class HelpCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        Player player = (Player) sender;
+        if (args.length == 0) {
+            TextComponent message = new TextComponent("Click here to see the plugin's wiki for help !");
+            message.setColor(ChatColor.GOLD);
+            message.setBold(true);
+            message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/thelordman/CoStrength/wiki"));
+            message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click here to be sent to the plugin's wiki").color(ChatColor.GRAY).create()));
+            player.spigot().sendMessage(message);
+            return true;
+        }
+        return false;
+    }
+}
