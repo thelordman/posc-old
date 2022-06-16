1765825471e7a4ea8d0ac42861eb702532a1c08f
Hunter-creator <101205562+thelordman@users.noreply.github.com>
2022-06-16 18:47:09 +0300

    Refined /spawn

--- src/main/java/io/github/thelordman/costrength/commands/SpawnCommand.java
+++ src/main/java/io/github/thelordman/costrength/commands/SpawnCommand.java
@@ -1,5 +1,6 @@
 package io.github.thelordman.costrength.commands;
 
+import io.github.thelordman.costrength.CoStrength;
 import io.github.thelordman.costrength.ranks.RankManager;
 import io.github.thelordman.costrength.utilities.Methods;
 import org.bukkit.Bukkit;
@@ -15,12 +16,21 @@ import org.jetbrains.annotations.NotNull;
 public class SpawnCommand implements CommandExecutor {
     public final boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
         if (sender instanceof ConsoleCommandSender && args.length == 0 | (!RankManager.hasPermission((OfflinePlayer) sender, (byte) 2)) && args.length > 0) return false;
+        if (Methods.inCombat((Player) sender)) {
+            sender.sendMessage(Methods.cStr("&cYou are combat tagged and cannot run that command."));
+            return true;
+        }
 
         Player target = args.length > 0 ? Bukkit.getPlayer(args[0]) : (Player) sender;
         if (target == null) return false;
 
-        Methods.teleportPlayerToSpawn(target, PlayerTeleportEvent.TeleportCause.COMMAND);
-        target.sendMessage(Methods.cStr("&6You have been sent to spawn. &f" + Methods.locToString(Bukkit.getWorld("world").getSpawnLocation(), false)));
+        Bukkit.getScheduler().runTaskLater(CoStrength.instance, () -> spawn(target), target == sender ? 100L : 0L);
+
         return true;
+    }
+
+    private void spawn(Player player) {
+        Methods.teleportPlayerToSpawn(player, PlayerTeleportEvent.TeleportCause.COMMAND);
+        player.sendMessage(Methods.cStr("&6You have been sent to spawn."));
     }
 }
