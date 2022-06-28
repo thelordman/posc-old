66135ce4e99873be97c5e37f7ca149eaefd27731
hotpocket184 <80357042+hotpocket184@users.noreply.github.com>
2022-06-28 14:13:48 -0400

    Added Staff Chat Command (it won't work it might be because i changed the java version to 17 instead of 16)

--- src/main/java/io/github/thelordman/costrength/utilities/CommandHandler.java
+++ src/main/java/io/github/thelordman/costrength/utilities/CommandHandler.java
@@ -38,6 +38,7 @@ public record CommandHandler(CoStrength plugin) {
         registerCommand("level", new LevelCommand());
         registerCommand("glow", new GlowCommand());
         registerCommand("pay", new PayCommand());
+        registerCommand("sc", new StaffChatCommand());
     }
 
     private void registerCommand(String command, CommandExecutor executor) {
