32d0237165f724a1f776603461e9775166d05b01
hotpocket184 <80357042+hotpocket184@users.noreply.github.com>
2023-04-15 15:55:52 -0400

    Sign edit thing

--- src/main/java/me/lord/posc/listeners/SignClickListener.java
+++ src/main/java/me/lord/posc/listeners/SignClickListener.java
@@ -3,13 +3,14 @@ package me.lord.posc.listeners;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class SignClickListener implements Listener {
 
     @EventHandler
     public void onSignClick(PlayerInteractEvent event) {
-        if (event.getClickedBlock() != null && event.getClickedBlock() instanceof Sign sign) {
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock() != null && event.getClickedBlock() instanceof Sign sign) {
             event.getPlayer().openSign(sign);
         }
     }
